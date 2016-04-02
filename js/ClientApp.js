08f1b44e84240b40b39f6d9a03a65b985483e031
WillJHaggard <willjhaggard@gmail.com>
2016-04-02 16:26:36 -0400

    Added first Video Component Prototype

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -2,13 +2,39 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = (props) => (
-  <div>
-    <h1>Hola from {props.location
-                          .split('')
-                          .reverse()
-                          .join('')
-                          .toUpperCase()}</h1>
-  </div>
+  <article className='list-performer guitar'>
+    <div className='header-video'>
+      <section className='video-container'>
+        <div className='overlay'>
+          <img className='poster' src='http://placehold.it/350x150' />
+          <img className='poster-colored' src='http://placehold.it/350x150' />
+          <a className='play-button' href='https://youtu.be/w3kOhMnNXnQ'>
+            <span className='before'></span>
+            <span className='after'></span>
+          </a>
+        </div>
+        <div className='votes-roses'>
+          <div className='votes'><span></span></div>
+          <div className='roses'><span></span></div>
+        </div>
+        <div className='player'></div>
+    </section>
+    <header>
+      <div className='info'>
+        <p className='type'>Guitar / Bass</p>
+        <p className='location'>Valencia/Spain</p>
+      </div>
+      <h1>
+        <a href='#'>Borja<br />Catanesi</a>
+      </h1>
+      <div className='performer-links'>
+        <a href='#' className='link'>Profile</a>
+        <a href='#' className='link'>View encore video</a>
+      </div>
+
+    </header>
+    </div>
+  </article>
 )
 
 const target = document.getElementById('app')
--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     filename: 'bundle.js'
   },
   resolve: {
-    extensions: ['', '.js', '.jsx', '.json']
+    extensions: ['', '.js', '.json']
   },
   stats: {
     colors: true,
